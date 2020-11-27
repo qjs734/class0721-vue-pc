9a342a7bf1c39d4c62fd1dce06351147f8e237c2
秦佳盛 <1337275169@qq.com>
2020-11-28 01:43:58 +0800

    feat:配置好less，Header组件初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 import router from './router/index' //引入router
 
+import './styles/reset.css' //引入公共资源
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
